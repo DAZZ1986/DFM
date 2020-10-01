469ab694dc4b4914fa47cd60147cbe14177ea430
DAZZ1986 <50179402+DAZZ1986@users.noreply.github.com>
2020-10-01 23:25:38 +0400

    minus stroka 1

--- DFM/Program.cs
+++ DFM/Program.cs
@@ -6,7 +6,7 @@ namespace DFM
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello !");
+            Console.WriteLine("Hello");
             Console.WriteLine("Hello 02222222222221!");
 
         }
