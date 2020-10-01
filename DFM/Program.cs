63a7ea7a277d7735dfb9c5e70f82c6ef7c8635fc
DAZZ1986 <50179402+DAZZ1986@users.noreply.github.com>
2020-10-01 22:56:02 +0400

    213123123123

--- DFM/Program.cs
+++ DFM/Program.cs
@@ -6,7 +6,7 @@ namespace DFM
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Wfdgdgorld!");
+            Console.WriteLine("Hello Wfdgdgorld! 000000");
         }
     }
 }
