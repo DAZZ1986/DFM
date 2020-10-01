c7d40a60e11b65dcd6dc7f05785ae0b6d7d9cb9f
DAZZ1986 <50179402+DAZZ1986@users.noreply.github.com>
2020-10-01 23:01:16 +0400

    добавил овую текстовую строку

--- DFM/Program.cs
+++ DFM/Program.cs
@@ -7,6 +7,8 @@ namespace DFM
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Wfdgdgorld!");
+            Console.WriteLine("Hello 00000000000000000000000000000000000!");
+
         }
     }
 }
