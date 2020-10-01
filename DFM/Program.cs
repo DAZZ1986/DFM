5bfb76792cc9bb9d3a55d787c56b64d3cb1b62e2
DAZZ <art.murtazov@gmail.com>
2020-10-01 23:51:13 +0400

    fix

--- DFM/Program.cs
+++ DFM/Program.cs
@@ -7,7 +7,7 @@ namespace DFM
         static void Main(string[] args)
         {
             Console.WriteLine("Hello");
-            Console.WriteLine("Hello 0222222222....2221!");
+            Console.WriteLine("Hello 0222222222....kkkkkkkkkkkkkkkkkkkkkkkkkkkk2221!");
             Console.WriteLine("Hello 02222222222221!");
             Console.WriteLine("Hello 02222222222221!");
             Console.WriteLine("Hello 02222222222221!");
